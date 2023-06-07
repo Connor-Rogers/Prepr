4f32722debfcb2b107a424ca406c403defba074e
rogersc7atwit <connorrog01@icloud.com>
2023-06-06 21:27:15 -0400

    BoilerPlate App.py

--- flask_apps/prepr_api/app.py
+++ flask_apps/prepr_api/app.py
@@ -1,1 +1,21 @@
 # Main WSGI Application
+import flask
+from flask import Blueprint
+
+
+def app_factory():
+    """
+    Create Flask app
+    """
+
+    app = flask.Flask(__name__)
+    app.config = {
+        "DEBUG": True
+    }
+
+    return app
+
+
+if __name__ == '__main__':
+    app = app_factory()
+    app.run()
