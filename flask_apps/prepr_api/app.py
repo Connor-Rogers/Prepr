23f12c5db1762b9edf2cc24bf7d79b3dd2d1d83b
rogersc7atwit <connorrog01@icloud.com>
2023-06-28 10:20:04 -0400

    Stuff

--- flask_apps/prepr_api/app.py
+++ flask_apps/prepr_api/app.py
@@ -1,6 +1,7 @@
 # Main WSGI Application
 import flask
 from flask import Blueprint
+from flask_cors import CORS
 
 
 def app_factory():
@@ -10,14 +11,29 @@ def app_factory():
 
     app = flask.Flask(__name__)
     app.config = {
-        "DEBUG": True
+        "SECRET_KEY": "secret",
+        "DEBUG": True,
+        "SERVER_NAME": "127.0.0.1:5000",
+        "PROPAGATE_EXCEPTIONS": True,
+        "APPLICATION_ROOT": "/v1",
+        "PREFERRED_URL_SCHEME": "http",
+        "SESSION_COOKIE_NAME": "session",
+        "SESSION_COOKIE_DOMAIN": ".localhost",
+        "SESSION_COOKIE_PATH": "/",
+        "SESSION_COOKIE_SECURE": False,
+        "SESSION_COOKIE_SAMESITE": "Lax",
+        "SESSION_REFRESH_EACH_REQUEST": True,
+        "SESSION_COOKIE_HTTPONLY": True,
+        "TRAP_HTTP_EXCEPTIONS": True,
     }
+    CORS(app, origins=["http://localhost:8100"])
     from bp.main_bp import main
+
     app.register_blueprint(main)
 
     return app
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = app_factory()
     app.run()
--- flask_apps/prepr_api/bp/main_bp.py
+++ flask_apps/prepr_api/bp/main_bp.py
@@ -1,9 +1,123 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from lib.iam import verify_token
+from lib.firebase import db
+
+main = Blueprint("main_bp", __name__)
 
 
-main = Blueprint('main_bp', __name__)
-
-
-@main.route('/')
+@main.route("/")
 def index():
     return "Hello World!"
+
+
+from flask import Flask, request, jsonify, _request_ctx_stack
+from jose import jwt
+import requests
+
+AUTH0_DOMAIN = "<YOUR_AUTH0_DOMAIN>"
+AUTH0_API_AUDIENCE = "<YOUR_AUTH0_API_AUDIENCE>"
+ALGORITHMS = ["RS256"]
+
+app = Flask(__name__)
+
+
+def get_jwt():
+    """Obtains the Access Token from the Authorization Header"""
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        return None
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        return None
+    elif len(parts) == 1:
+        return None
+    elif len(parts) > 2:
+        return None
+
+    token = parts[1]
+    return token
+
+
+def verify_user(f):
+    """Determines if the Access Token is valid"""
+
+    def wrapper(*args, **kwargs):
+        token = get_jwt()
+        if token is None:
+            return (
+                jsonify(
+                    {
+                        "code": "authorization_header_missing",
+                        "description": "Authorization header is expected",
+                    }
+                ),
+                401,
+            )
+
+        jsonurl = requests.get(f"https://{AUTH0_DOMAIN}/.well-known/jwks.json")
+        jwks = jsonurl.json()
+        unverified_header = jwt.get_unverified_header(token)
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"],
+                }
+
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience=AUTH0_API_AUDIENCE,
+                    issuer=f"https://{AUTH0_DOMAIN}/",
+                )
+            except Exception as e:
+                return jsonify({"code": "invalid_header", "description": str(e)}), 401
+
+            _request_ctx_stack.top.current_user = payload
+            return f(*args, **kwargs)
+
+        return (
+            jsonify(
+                {
+                    "code": "invalid_header",
+                    "description": "Unable to find the appropriate key",
+                }
+            ),
+            401,
+        )
+
+    return wrapper
+
+
+@app.route("/v1/secure/submit-macros", methods=["POST"])
+@verify_user
+def submit_macros():
+    # Example: do something with the posted data
+    data = request.json
+    return jsonify({"message": "Data received", "data": data})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
+# @main.route("/v1/secure/submit-macros", methods=["POST"])
+# @verify_token
+# def submit_macros(payload):
+#     # You can access user information from the payload
+#     user_id = payload.get("sub")  # sub usually contains the user id
+
+#     data = request.json
+#     # Save data to Firebase with user information
+#     macros_ref = db.collection("macro_calc")
+#     macros_ref.add({"user_id": user_id, "macros": data})
+
+#     return jsonify({"message": "Data saved successfully"}), 200
