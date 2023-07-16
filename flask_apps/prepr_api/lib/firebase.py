45258eeb90128cb6ac2ef08c9cb6cc334a7536d5
rogersc7atwit <connorrog01@icloud.com>
2023-07-16 17:45:08 -0400

    Small Changes

--- flask_apps/prepr_api/lib/firebase.py
+++ flask_apps/prepr_api/lib/firebase.py
@@ -1,9 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from google.cloud import storage
+import json
 
 cred = credentials.Certificate("prepr_fb_secret.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-
+buckit = storage.Client.from_service_account_json("prepr_fb_secret.json")
 
 ## Aggregation Functions
