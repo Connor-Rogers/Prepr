7892a70e71978b8234020c5721c60df76090df86
rogersc7atwit <connorrog01@icloud.com>
2023-08-02 22:42:47 -0400

    Added Recipe Feed Forward to generator

--- flask_apps/prepr_api/bp/main_bp.py
+++ flask_apps/prepr_api/bp/main_bp.py
@@ -3,6 +3,8 @@ from lib.iam import firebase_auth_required
 from lib.macro import macro_calculator
 from werkzeug.datastructures import FileStorage
 import openai
+import random
+from lib.firebase import buckit, db
 
 main = Blueprint("main_bp", __name__)
 
@@ -12,28 +14,25 @@ def index():
     return "Hello World!"
 
 
+def get_random_recipe():
+    # Fetch all recipes
+    recipes_ref = db.collection("recipes")
+    docs = recipes_ref.get()
+
+    # Convert all recipes to a list
+    recipes = [doc.to_dict() for doc in docs]
+
+    # Choose a random recipe
+    random_recipe = random.choice(recipes)
+    return random_recipe
+
+
 def generate_recipe():
     openai.api_key = "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"
 
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="""Please generate a detailed recipe including the title, ingredients, instructions, and macro nutritional information.
-        The recipe should be formatted as follows:
-        Title: The name of the dish
-        Ingredients:
-        - Ingredient 1
-        - Ingredient 2
-        - Ingredient 3
-        - ...
-        Instructions:
-        1. First instruction
-        2. Second instruction
-        3. ...
-        Macros:
-        Calories: X kcal
-        Protein: Y g
-        Carbs: Z g
-        Fat: W g
+        prompt=f"""Please generate a detailed recipe including the title, ingredients, instructions, and macro nutritional information. Use the following recipe as a template and should be formated as a json: {get_random_recipe()}
         """,
         temperature=0.3,
         max_tokens=600,
@@ -50,10 +49,10 @@ def generate_recipe():
 @main.route("/gen<days>", methods=["GET", "POST"])
 # @firebase_auth_required
 def gen_meal_plan(days):
-    days = int(days)
+    days = 1
     meal_plan = {}
 
     for i in range(days):
-        meal_plan[f"Day {i+1}"] = generate_recipe()
+        meal_plan = generate_recipe()
 
-    return jsonify(meal_plan)
+    return jsonify(meal_plan), 200
