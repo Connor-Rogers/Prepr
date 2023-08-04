863494970b9ab285b8b8383b202f3693841db28b
rogersc7atwit <connorrog01@icloud.com>
2023-08-03 21:54:12 -0400

    HELLO

--- flask_apps/prepr_api/bp/main_bp.py
+++ flask_apps/prepr_api/bp/main_bp.py
@@ -1,3 +1,5 @@
+import ast
+import json
 from flask import Blueprint, request, jsonify, g
 from lib.iam import firebase_auth_required
 from lib.macro import macro_calculator
@@ -29,11 +31,23 @@ def get_random_recipe():
 
 def generate_recipe():
     openai.api_key = "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"
-
+    prompt = (
+        """Please generate a detailed recipe including the title, ingredients, instructions, and macro nutritional information. Use the following recipe as a template """
+        + str(get_random_recipe())
+        + """ and should be formated as a json with the following feilds: 
+    title: "title of recipe"
+    ingredients: [{"ingredient": "ingredient name", "quantity": "quantity unit"}, {"ingredient": "ingredient name", "quantity": "quantity unit"}]] 
+    instructions: "Instructions for recipe"
+    calories: "calories"`
+    fats: "fats"
+    carbs: "carbs"
+    proteins: "proteins"
+    """
+    )
+    print(prompt)
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f"""Please generate a detailed recipe including the title, ingredients, instructions, and macro nutritional information. Use the following recipe as a template and should be formated as a json: {get_random_recipe()}
-        """,
+        prompt=prompt,
         temperature=0.3,
         max_tokens=600,
         top_p=1.0,
@@ -53,6 +67,5 @@ def gen_meal_plan(days):
     meal_plan = {}
 
     for i in range(days):
-        meal_plan = generate_recipe()
-
+        meal_plan = json.loads(generate_recipe(), strict=False)
     return jsonify(meal_plan), 200
