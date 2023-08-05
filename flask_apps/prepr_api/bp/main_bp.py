98d372205c0ac9e1f071f79e24b6d56c42b47800
tristam-sanborn <sanbornt1@wit.edu>
2023-08-05 11:20:42 -0400

    Added in pantry items

--- flask_apps/prepr_api/bp/main_bp.py
+++ flask_apps/prepr_api/bp/main_bp.py
@@ -15,6 +15,15 @@ main = Blueprint("main_bp", __name__)
 def index():
     return "Hello World!"
 
+def get_random_recipe_from_likes(user_id):
+    likes_ref = db.collection("likes").document(user_id)
+    likes_doc = likes_ref.get()
+
+    # If the document doesn't exist, create a new dictionary. Otherwise, get the data from the existing document
+    likes = likes_doc.to_dict() if likes_doc.exists else {}
+
+    random_liked = random.choice(likes)
+    return random_liked
 
 def get_random_recipe():
     # Fetch all recipes
@@ -28,12 +37,29 @@ def get_random_recipe():
     random_recipe = random.choice(recipes)
     return random_recipe
 
+#add to recipe database button for when gpt makes a recipe
+#Add back pantry items to prompts when included
+#gen 3 recipes when page loads tiles for recipe will have name macros and pics
+#upgrade css
+
 
 def generate_recipe():
     openai.api_key = "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"
+    #if pantryItems != null: 
+
+    data = request.get_json()
+    pantry_items = data.get("pantryItems")
+    if pantry_items:
+        num_to_sample = len(pantry_items)%3
+        if num_to_sample == 0: num_to_sample = 1
+        num_items = min(random.randint(1, num_to_sample), len(pantry_items))
+        items_for_day = random.sample(pantry_items, num_items)
+        pantry_items = [item for item in pantry_items if item not in items_for_day]
     prompt = (
-        """Please generate a detailed recipe including the title, ingredients, instructions, and macro nutritional information. Use the following recipe as a template """
-        + str(get_random_recipe())
+        """Please generate a detailed recipe including the title, ingredients, instructions, and macro nutritional information. Use the following recipe as a template and inspiration """
+        + str(get_random_recipe_from_likes())
+        + """ incorporate the following additional ingredients into the recipe """
+        + str(items_for_day)
         + """ and should be formated as a json with the following feilds: 
     title: "title of recipe"
     ingredients: [{"ingredient": "ingredient name", "quantity": "quantity unit"}, {"ingredient": "ingredient name", "quantity": "quantity unit"}]] 
@@ -56,6 +82,7 @@ def generate_recipe():
     )
 
     recipe = response.choices[0].text.strip()
+    print (prompt)
     print(recipe)
     return recipe
 
