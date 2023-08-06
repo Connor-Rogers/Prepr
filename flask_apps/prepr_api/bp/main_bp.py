fe7d1d0b1af79356ee6272c50ba7da1670e19f0c
tristam-sanborn <sanbornt1@wit.edu>
2023-08-06 12:15:38 -0400

    updated generation code

--- flask_apps/prepr_api/bp/main_bp.py
+++ flask_apps/prepr_api/bp/main_bp.py
@@ -7,6 +7,10 @@ from werkzeug.datastructures import FileStorage
 import openai
 import random
 from lib.firebase import buckit, db
+from datetime import timedelta
+import io, re
+import datetime
+import uuid
 
 main = Blueprint("main_bp", __name__)
 
@@ -50,16 +54,30 @@ def generate_recipe():
     data = request.get_json()
     pantry_items = data.get("pantryItems")
     if pantry_items:
-        num_to_sample = len(pantry_items)%3
+        num_to_sample = random.randint(0, len(pantry_items))
         if num_to_sample == 0: num_to_sample = 1
         num_items = min(random.randint(1, num_to_sample), len(pantry_items))
         items_for_day = random.sample(pantry_items, num_items)
         pantry_items = [item for item in pantry_items if item not in items_for_day]
-    prompt = (
+        prompt = (
         """Please generate a detailed recipe including the title, ingredients, instructions, and macro nutritional information. Use the following recipe as a template and inspiration """
         + str(get_random_recipe_from_likes())
         + """ incorporate the following additional ingredients into the recipe """
         + str(items_for_day)
+        + """ and should be formated as a json with the following feilds: 
+    title: "title of recipe"
+    ingredients: [{"ingredient": "ingredient name", "quantity": "quantity unit"}, {"ingredient": "ingredient name", "quantity": "quantity unit"}]] 
+    instructions: "Instructions for recipe"
+    calories: "calories"`
+    fats: "fats"
+    carbs: "carbs"
+    proteins: "proteins"
+    """
+    )
+    else:
+        prompt = (
+        """Please generate a detailed recipe including the title, ingredients, instructions, and macro nutritional information. Use the following recipe as a template and inspiration """
+        + str(get_random_recipe_from_likes())
         + """ and should be formated as a json with the following feilds: 
     title: "title of recipe"
     ingredients: [{"ingredient": "ingredient name", "quantity": "quantity unit"}, {"ingredient": "ingredient name", "quantity": "quantity unit"}]] 
@@ -86,13 +104,77 @@ def generate_recipe():
     print(recipe)
     return recipe
 
+def store_gpt_recipe(recipe):
+    recipe = json.loads(recipe)
+    # get the form data and convert it to a dictionary
+    form_data = dict(recipe)
+
+    # extract all keys that start with 'ingredients'
+    ingredient_keys = [k for k in form_data.keys() if k.startswith("ingredients")]
+
+    # initialize an empty list to hold your ingredients
+    ingredients = []
+
+    # process each ingredient key
+    for k in ingredient_keys:
+        # split the key into parts
+        parts = k.split("[")
+
+        # extract the index and property name
+        index = int(parts[1][:-1])  # remove the trailing ']'
+        prop = parts[2][:-1]  # remove the trailing ']'
+
+        # extend the list of ingredients if necessary
+        while index >= len(ingredients):
+            ingredients.append({})
+        # add the property to the appropriate ingredient
+        ingredients[index][prop] = form_data[k]
+
+    # print the list of ingredients
+    print(ingredients)
+
+    title = form_data.get("title")
+    instructions = form_data.get("instructions")
+    calories = form_data.get("calories")
+    fats = form_data.get("fats")
+    carbs = form_data.get("carbs")
+    proteins = form_data.get("proteins")
+
+
+    # creating a unique id for the recipe
+    recipe_id = str(uuid.uuid4())
+
+    bucket = buckit.get_bucket("prepr-391015.appspot.com")
+
+    doc_ref = db.collection("recipes").document(recipe_id)
+
+    doc_ref.set(
+        {
+            "author": "GPT Chef",
+            "date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "title": title,
+            "instructions": instructions,
+            "calories": calories,
+            "fats": fats,
+            "carbs": carbs,
+            "proteins": proteins,
+            "ingredients": ingredients,  # use the processed list of ingredients
+            "photos": "",
+        }
+    )
+
+    return jsonify({"message": "Recipe created successfully"}), 200
+
+
 
 @main.route("/gen<days>", methods=["GET", "POST"])
-# @firebase_auth_required
+@firebase_auth_required
 def gen_meal_plan(days):
-    days = 1
-    meal_plan = {}
+    days = int(days)  # convert days to integer
+    meal_plan = []
 
     for i in range(days):
-        meal_plan = json.loads(generate_recipe(), strict=False)
+        recipe = json.loads(generate_recipe(), strict=False)
+        meal_plan.append(recipe)
     return jsonify(meal_plan), 200
+
