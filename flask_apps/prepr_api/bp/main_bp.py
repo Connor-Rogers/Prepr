e5100aa6cf4cdc825f8cc4953bb66e33f9318361
rogersc7atwit <connorrog01@icloud.com>
2023-08-07 20:07:31 -0400

    Seprated Recipe Generation Methods from blueprint.

--- flask_apps/prepr_api/bp/main_bp.py
+++ flask_apps/prepr_api/bp/main_bp.py
@@ -1,112 +1,19 @@
 import datetime, uuid
 import json
 from flask import Blueprint, request, jsonify, g
-from lib.iam import firebase_auth_required
-from lib.macro import macro_calculator
-from werkzeug.datastructures import FileStorage
-import openai
-import random
-import ast
-from lib.firebase import buckit, db
-from datetime import timedelta
-import io, re
+from lib.firebase import db
 import datetime
 import uuid
+
+from lib.iam import firebase_auth_required
+from lib.recpie_gen import generate_recipe
 
 main = Blueprint("main_bp", __name__)
 
 
 @main.route("/")
 def index():
-    return "Hello World!"
-
-
-def get_random_recipe_from_likes(user_id):
-    # Find recipes liked by the user
-    liked_recipes = []
-    likes_docs = db.collection("likes").stream()
-
-    for doc in likes_docs:
-        likes = doc.to_dict().get("likes", [])
-        if user_id in likes:
-            liked_recipes.append(doc.id)
-
-    # If the user has liked any recipes, pick one at random
-    if liked_recipes:
-        random_recipe_id = random.choice(liked_recipes)
-    else:
-        # If user hasn't liked any recipe, pick a random recipe from all recipes
-        all_recipe_ids = [doc.id for doc in db.collection("recipes").stream()]
-        if not all_recipe_ids:
-            return {}  # Return empty dict if there are no recipes in the database
-        random_recipe_id = random.choice(all_recipe_ids)
-
-    # Get the recipe details from the "recipes" collection
-    recipe_doc = db.collection("recipes").document(random_recipe_id).get()
-    if recipe_doc.exists:
-        return recipe_doc.to_dict()
-    return {}
-
-
-# add to recipe database button for when gpt makes a recipe
-# Add back pantry items to prompts when included
-# gen 3 recipes when page loads tiles for recipe will have name macros and pics
-# upgrade css
-
-
-def generate_recipe(user_id, pantry_items: list = None):
-    openai.api_key = "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"
-    # if pantryItems != null:
-    items_for_day = []
-    if pantry_items:
-        num_to_sample = random.randint(0, len(pantry_items))
-        if num_to_sample == 0:
-            num_to_sample = 1
-        num_items = min(random.randint(1, num_to_sample), len(pantry_items))
-        items_for_day = random.sample(pantry_items, num_items)
-        pantry_items = [item for item in pantry_items if item not in items_for_day]
-        prompt = (
-            """Please generate a detailed recipe including the title, ingredients, instructions, and macro nutritional information. Use the following recipe as a template and inspiration """
-            + str(get_random_recipe_from_likes(user_id))
-            + """ incorporate the following additional ingredients into the recipe """
-            + str(items_for_day)
-            + """ and should be formated as a json with the following feilds: 
-    title: "title of recipe"
-    ingredients: [{"ingredient": "ingredient name", "quantity": "quantity unit"}, {"ingredient": "ingredient name", "quantity": "quantity unit"}]] 
-    instructions: "Instructions for recipe"
-    calories: "calories"`
-    fats: "fats"
-    carbs: "carbs"
-    proteins: "proteins"
-    """
-        )
-    else:
-        prompt = (
-            """Please generate a detailed recipe including the title, ingredients, instructions, and macro nutritional information. Use the following recipe as a template and inspiration """
-            + str(get_random_recipe_from_likes())
-            + """ and should be formated as a json with the following feilds: 
-    title: "title of recipe"
-    ingredients: [{"ingredient": "ingredient name", "quantity": "quantity unit"}, {"ingredient": "ingredient name", "quantity": "quantity unit"}]] 
-    instructions: "Instructions for recipe"
-    calories: "calories"`
-    fats: "fats"
-    carbs: "carbs"
-    proteins: "proteins"
-    """
-        )
-
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=0.3,
-        max_tokens=600,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-    )
-
-    recipe = response.choices[0].text.strip()
-    return recipe
+    return "Prepr API Beta"
 
 
 @main.route("/gen<int:days>", methods=["GET", "POST"])
